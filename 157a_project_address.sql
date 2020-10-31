dc563179fc60bc8ee4036d4edb1830b077935ec2
Tai Pham <54519761+Tai-Pham@users.noreply.github.com>
2020-10-30 19:17:18 -0700

    Update 157a_project_address.sql

--- 157a_project_address.sql
+++ 157a_project_address.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `address`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `address` (
-  `Address ID` int NOT NULL,
+  `Address ID` int NOT NULL AUTO_INCREMENT,
   `City ID` varchar(45) DEFAULT NULL,
   `State ID` varchar(45) DEFAULT NULL,
   `Address` varchar(45) DEFAULT NULL,
